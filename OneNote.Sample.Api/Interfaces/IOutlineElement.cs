2b1e5f18d2c275381f1adae50dedfb96a4aabb0e
Victor Birin <victor.birin@gmail.com>
2020-05-03 18:43:19 +0300

    Optimized IOutlineElement interface

--- OneNote.Sample.Api/Interfaces/IOutlineElement.cs
+++ OneNote.Sample.Api/Interfaces/IOutlineElement.cs
@@ -1,20 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Printing;
-
-namespace OneNote.Sample.Api
+﻿namespace OneNote.Sample.Api
 {
     public interface IOutlineElement : IElement
     {
         ElementType ElementType { get; }
-        Font Font { get; set; }
-        float FontSize { get; set; }
-        Color Color { get; set; }
-        Margins Margins { get; set; }
-        string Text { get; set; }
         string XPath { get; set; }
-
-        Dictionary<string, string> Attributes { get; set; }
-        Dictionary<string, string> Styles { get; set; }
     }
 }
